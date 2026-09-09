c2796d464987d4b96cd3ebafd4a4177be5ad7f6f
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-09 14:55:21 +0200

    Fix inclusive banlist page size

--- src/Extractors/Banlist/DefaultExtractor.php
+++ src/Extractors/Banlist/DefaultExtractor.php
@@ -67,7 +67,7 @@ class DefaultExtractor extends Extractor
                 ->filter()
                 ->values(),
             total: $pagination['total'],
-            perPage: $pagination['end'] - $pagination['start'],
+            perPage: $pagination['end'] - $pagination['start'] + 1,
             currentPage: $this->currentPageFromSelect($crawler->filter(' #banlist-nav select')),
         );
     }
