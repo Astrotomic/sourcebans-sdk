9cad25214b98edea42d9beb51bd9bcc5aaed5692
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-09 14:58:10 +0200

    Keep SourceBans offset page size semantics

--- src/Extractors/Banlist/DefaultExtractor.php
+++ src/Extractors/Banlist/DefaultExtractor.php
@@ -67,7 +67,7 @@ class DefaultExtractor extends Extractor
                 ->filter()
                 ->values(),
             total: $pagination['total'],
-            perPage: $pagination['end'] - $pagination['start'] + 1,
+            perPage: $pagination['end'] - $pagination['start'],
             currentPage: $this->currentPageFromSelect($crawler->filter(' #banlist-nav select')),
         );
     }
